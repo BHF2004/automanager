e010d3257c107e77100e6f531634671df1b6b4d5
Eren Cetinbas <eren.bilal1510@googlemail.com>
2023-12-03 20:31:42 +0100

    Add one car

--- auto.java
+++ auto.java
@@ -1,5 +1,15 @@
 class Auto {
+    String marke;
+
     public static void main(String[] args) {
         System.out.println("Willommen zum Automanager");
+        String auto1Marke;
+        int auto1baujahr;
+        auto1Marke = "Tesla";
+        auto1baujahr = 2023;
+        System.out.println("Marke:" + auto1Marke);
+        System.out.println("Baujahr:" + auto1baujahr);
+
     }
+
 }
